39454220193e1c73e5a7f5e32cc2702d4f46d26d
debomita29 <debomitachakraborty2999@gmail.com>
2022-08-03 14:52:29 +0530

    final product

---
+++ eator_fe/src/App.js
@@ -0,0 +1,19 @@
+//import Banner from './components/Banner'
+import Navigation from "./components/Navigation";
+function App() {
+  return (
+   <Navigation/>
+  );
+}
+
+export default App;
+/* <button type="button" 
+            className="btn btn-primary" 
+            data-bs-toggle="modal" 
+            data-bs-target="#loginModal">
+                Launch Login Form
+      </button>
+
+      <Modal modalid="loginModal"  modaltitle="Login">
+          <Login/>
+      </Modal>  */
